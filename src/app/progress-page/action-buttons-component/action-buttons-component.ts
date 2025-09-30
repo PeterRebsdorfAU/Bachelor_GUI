4873208b6f0673d22aabd117acad7983c73f482e
PeterRebsdorf <202206699@post.au.dk>
2025-09-30 12:48:38 +0200

    Fixed styling of progress page

--- src/app/progress-page/action-buttons-component/action-buttons-component.ts
+++ src/app/progress-page/action-buttons-component/action-buttons-component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import { ProgressOverviewService } from '../progress-overview-service';
 import { UserRole } from '../../user-role.enum';
 import { NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-action-buttons-component',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, MatButtonModule, MatIconModule],
   templateUrl: './action-buttons-component.html',
   styleUrl: './action-buttons-component.scss'
 })
--- src/app/progress-page/checklist-component/checklist-component.ts
+++ src/app/progress-page/checklist-component/checklist-component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import {NgFor} from '@angular/common';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-checklist-component',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, MatListModule],
   templateUrl: './checklist-component.html',
   styleUrl: './checklist-component.scss'
 })
