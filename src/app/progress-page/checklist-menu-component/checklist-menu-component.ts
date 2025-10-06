f77af459cf9fb3cf9d88a2d88e203c86a8f20845
PeterRebsdorfAU <202206699@post.au.dk>
2025-10-06 19:44:06 +0200

    Fixed the menu on progress page

--- src/app/progress-page/checklist-menu-component/checklist-menu-component.ts
+++ src/app/progress-page/checklist-menu-component/checklist-menu-component.ts
@@ -1,18 +1,24 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {NgFor} from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-checklist-menu-component',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf, NgClass, MatListModule, MatIconModule],
   templateUrl: './checklist-menu-component.html',
   styleUrl: './checklist-menu-component.scss'
 })
 export class ChecklistMenuComponent {
-  @Input() items: any[] = [];
+  @Input() sections: any[] = [];
   @Output() itemSelected = new EventEmitter<any>();
 
   selectItem(item: any) {
     this.itemSelected.emit(item);
+  }
+
+  allSubItemsChecked(item: any): boolean {
+    return !!item.isChecked;
   }
 }
